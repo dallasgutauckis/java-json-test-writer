4821fcf176b04ef91d1022217fdc2971f9fb4aa5
Dallas Gutauckis <dallas@gutauckis.com>
2015-02-27 01:20:33 -0500

    Adding tracking and ads

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
       <div class="content">
         <h1>java-json-test-writer</h1>
         <em>by <a href="http://dallasgutauckis.com">Dallas Gutauckis</a></em>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- java-json-test-writer header -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:728px;height:90px"
+             data-ad-client="ca-pub-7874761747975813"
+             data-ad-slot="9345280686"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         <form method="post">
           <fieldset>
             <div class="row">
@@ -94,5 +103,15 @@
         </pre>
       </div>
     </div>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-401905-9', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 </html>
